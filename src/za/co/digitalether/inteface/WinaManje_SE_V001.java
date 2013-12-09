15e727f48d80a3aa83935be0f401d3798ed90d10
tsmobu <tshepo@digitalether.co.za>
2013-12-09 20:35:24 +0200

    the rest of the project code
    lets see if all goes there

---
+++ src/za/co/digitalether/inteface/WinaManje_SE_V001.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package za.co.digitalether.inteface;
+
+/**
+ *
+ * @author Tshepo
+ */
+public class WinaManje_SE_V001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        WinaFrame wf = new WinaFrame();
+        wf.setVisible(true);
+    }
+    
+}
